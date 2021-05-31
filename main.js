9976d2aed25651c2f3340ba3a3509b524f879c45
SamvegTiwari <74610526+SamvegTiwari@users.noreply.github.com>
2021-05-31 09:53:05 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,68 @@
+
+
+function buy(){
+    window.location="grade1_buy.html";
+}
+
+function exchange(){
+    window.location="exchange1.html";
+}
+
+function Submitinfo(){
+  window.location="phonenumber.html" ;
+}
+
+function finish(){
+ window.location="Thankyou.html";
+}
+
+function buy2(){
+window.location="grade2_buy.html";
+}
+
+function buy3(){
+    window.location="grade3_buy.html";  
+}
+
+function buy4(){
+    window.location="grade4.buy.html";    
+}
+function buy5(){
+    window.location="grade5_buy.html";    
+}
+
+function buy6(){
+    window.location="grade6_buy.html";    
+}
+
+function buy7(){
+    window.location="grade7_buy.html";    
+}
+
+function buy8(){
+    window.location="grade8_buy.html";    
+}
+
+function exchange(){
+  window.location="exchange1.html";
+}
+
+function Buybookscience(){
+ window.location="buybook_science.html";
+}
+
+function Buyatlast(){
+window.location="finishBuy.html";   
+}
+
+function finish(){
+  window.location="index.html";  
+}
+
+function Buybookmath(){
+ window.location="buybook_math.html";  
+}
+
+function Buybookenglish(){
+    window.location="buybook_english.html";  
+   }
